9a0aab9c718b8be49e1c7a4f8ce911a4e4c5c0a5
Ubuntu <ubuntu@ip-10-0-0-252.ec2.internal>
2021-12-07 04:46:08 +0000

    added results

--- 2021.4/runs/03_module_shifting.sh
+++ 2021.4/runs/03_module_shifting.sh
@@ -6,4 +6,4 @@
 
 BIN_PATH='/home/ubuntu/genetic-circuit-partitioning/2021.4/bin'
 
-python3 $BIN_PATH/3_partition_optimize.py -settings ./settings.txt -samples 4input_3310,4input_30304,4input_17385,4input_39513,4input_16418,5input_71517,5input_54207,5input_42709,5input_23439,5input_36413,8input_83,6input_99,5input_90460,7input_86,6input_49,8input_87,6input_69,6input_88,6input_15,6input_30,6input_26,6input_72,6input_5,6input_67,6input_14,7input_59,8input_46,7input_87_c5,6input_54_c5,7input_36_c5,7input_26,8input_80_c5,7input_65_c5,7input_1_c5,8input_6_c5,7input_80_c5,7input_95_c5,7input_83,7input_72_c5,7input_71_c5,7input_33,7input_15_c5,7input_45_c5,7input_69_c5,7input_54_c5,7input_89_c5,7input_90_c5,8input_70_c5,7input_19_c5,7input_2_c5
+python3 $BIN_PATH/3_partition_optimize.py -settings ./settings.txt -samples 7input_87_c5,6input_54_c5,7input_36_c5,8input_80_c5,7input_65_c5,7input_1_c5,8input_6_c5,7input_80_c5,7input_95_c5,7input_72_c5,7input_71_c5,7input_15_c5,7input_45_c5,7input_69_c5,7input_54_c5,7input_89_c5,7input_90_c5,8input_70_c5,7input_19_c5,7input_2_c5
