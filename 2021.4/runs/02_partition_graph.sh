7b71e363f7bde5707cfafc2435b9e14a1d7e3b7b
Ubuntu <ubuntu@ip-10-0-0-124.ec2.internal>
2021-12-16 08:00:15 +0000

    add MIN result

--- 2021.4/runs/02_partition_graph.sh
+++ 2021.4/runs/02_partition_graph.sh
@@ -8,4 +8,4 @@
 
 BIN_PATH='/home/ubuntu/genetic-circuit-partitioning/2021.4/bin'
 
-python3 $BIN_PATH/2_graph_partition_bionetworks.py -settings ./settings_bionetwork.txt -samples SWG_n60_k4_p0.3,SWG_n60_k4_p0.4,SWG_n70_k4_p0.3,SWG_n70_k4_p0.4,SWG_n80_k4_p0.3,SWG_n80_k4_p0.4,SWG_n90_k4_p0.3,SWG_n90_k4_p0.4,SWG_n100_k4_p0.3,SWG_n100_k4_p0.4
+python3 $BIN_PATH/2_graph_partition_bionetworks.py -settings ./settings_bionetwork.txt -samples MIN
