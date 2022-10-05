dc0ee2e44d9fbcce5bcf7d27cd2041b7f7a896b1
Jing Zhang <jillwill00@gmail.com>
2022-10-05 15:26:30 -0400

    update

--- 2021.4/runs/03_RG_n70.sh
+++
@@ -1,9 +0,0 @@
-
-#	Copyright (C) 2020 by
-#	Jing Zhang <jgzhang@bu.edu>, Densmore Lab, Boston University
-# 	All rights reserved.
-#	OSI Non-Profit Open Software License ("Non-Profit OSL") 3.0 license.
-
-BIN_PATH='/home/ubuntu/genetic-circuit-partitioning/2021.4/bin'
-
-python3 $BIN_PATH/3_partition_optimize_bionetworks.py -settings ./settings_bionetwork.txt -samples RG_n70_p0.02
--- 2021.4/runs/03_module_shifting_batch7-1.sh
+++
@@ -1,9 +0,0 @@
-
-#	Copyright (C) 2020 by
-#	Jing Zhang <jgzhang@bu.edu>, Densmore Lab, Boston University
-# 	All rights reserved.
-#	OSI Non-Profit Open Software License ("Non-Profit OSL") 3.0 license.
-
-BIN_PATH='/home/ubuntu/genetic-circuit-partitioning/2021.4/bin'
-
-python3 $BIN_PATH/3_partition_optimize.py -settings ./settings.txt -samples 7input_90,8input_70,7input_19,7input_2
